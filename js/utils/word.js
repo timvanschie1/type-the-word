4118339c446d8833183620510031f68dcbbcb86a
timvanschie1 <timvanschie1@gmail.com>
2025-12-20 23:50:01 +0100

    six seven animation

--- js/utils/word.js
+++ js/utils/word.js
@@ -18,14 +18,14 @@ export function loadWordItems(mode) {
  */
 export function getWordsFileName(mode) {
   if (mode === 'youngKids') {
-    return 'wordsPerImageYoungKids.json?v=8';
+    return 'wordsPerImageYoungKids.json?v=9';
   }
 
   if (mode === 'brainrot') {
-    return 'wordsPerImageBrainrot.json?v=8';
+    return 'wordsPerImageBrainrot.json?v=9';
   }
 
-  return "wordsDutch.json?v=8";
+  return "wordsDutch.json?v=9";
 }
 
 /**
