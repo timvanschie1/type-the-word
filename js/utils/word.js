6e6f5a6c4a3820c49209d8a4f40bfa45cbc6ffcd
timvanschie1 <timvanschie1@gmail.com>
2025-12-25 15:05:26 +0100

    bump version

--- js/utils/word.js
+++ js/utils/word.js
@@ -43,14 +43,14 @@ export function getWordsFileName() {
   const mode = getMode();
 
   if (mode === 'youngKids') {
-    return 'wordsPerImageYoungKids.json?v=14';
+    return 'wordsPerImageYoungKids.json?v=15';
   }
 
   if (mode === 'brainrot') {
-    return 'wordsPerImageBrainrot.json?v=14';
+    return 'wordsPerImageBrainrot.json?v=15';
   }
 
-  return "wordsDutch.json?v=14";
+  return "wordsDutch.json?v=15";
 }
 
 /** @param {string} inputValue - The text from the input field. **/
